c26a40a865578336fdc73f6f7f8d9a1b448bf0ad
ceunnseo <eunseo4068@gmail.com>
2024-03-18 10:48:44 +0900

    날씨 api 호출하여 현재 위치의 날씨 가져오기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect } from 'react';
+const API_KEY = `073de8edeeb6cfc808889bd776923a93`
+/*
+1. 앱이 실행되자마자 현재 위치 기반의 날씨가 보인다.
+*/
 function App() {
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      const url = new URL(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`)
+      const response = await fetch(url)
+      const data = await response.json();
+      console.log(response, data);
+      console.log(data.main.temp); //현재 위치의 섭씨 온도
+    })
+  }
+  useEffect(()=> {
+    getCurrentLocation()
+  }, []) //componentDidMount -> 렌더하자마자 바로 실행된다.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      hi
     </div>
   );
 }
