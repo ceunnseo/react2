c73d549613c781249313ad7479b1ba0c11c99436
ceunnseo <eunseo4068@gmail.com>
2024-03-23 09:25:31 +0900

    html 수정

--- src/components/WeatherBox.js
+++ src/components/WeatherBox.js
@@ -1,14 +1,33 @@
 import React from 'react'
 
 const WeatherBox = ({weather}) => {
+  let today = new Date();
+  let month = today.getMonth() + 1;
+  let date = today.getDate();
+  let day = today.getDay();
+  if (day === 0) { day = 'Sun'}
+  else if (day === 1) {day = 'Mon'}
+  else if (day === 2) {day = 'Tue'}
+  else if (day === 3) {day = 'Wen'}
+  else if (day === 4) {day = 'Tru'}
+  else if (day === 5) {day = 'Fri'}
+  else if (day === 6) {day = 'Sat'}
   return (
     <div className = "weather-box">
-      <div id = "loc">{weather?.name||'정보를 불러올 수 없습니다.'}</div>
+      <div>Today {month}/{date} {day}</div>
       <div>
-        <img src = {`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}/>
+        <img id = "weatherIcon" src = {`https://openweathermap.org/img/wn/${weather && weather.weather[0].icon}.png`}/>
       </div>
-      <div>{weather && weather?.main.temp+'°C'}</div>
-      <div>{weather && weather?.weather[0].description}</div>
+      <div id = "temp_and_desc">
+        <div id = "temp_box">
+          <div id = "temp">{weather && Math.floor(weather?.main.temp)+'°C'}</div>
+          <div id = "temp_max">{weather && Math.floor(weather?.main.temp_max)+'°C'}  {weather && Math.floor(weather?.main.temp_min)+'°C'}</div>
+        </div>
+        <div id = "desc">
+          <div id = "loc">{weather?.name||'정보를 불러올 수 없습니다.'}</div>
+          <div>{weather && weather?.weather[0].description}</div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/WeatherButton.js
+++ src/components/WeatherButton.js
@@ -4,6 +4,7 @@ import { Button } from 'react-bootstrap';
 const WeatherButton = ({cities, setCity, getCurrentLocation }) => {
   return (
     <div>
+      <div>more</div>
       <Button variant="warning" onClick={()=>getCurrentLocation()}>current</Button>
       {cities.map((item) => (
         <Button variant="warning" key = {item} onClick={()=>setCity(item)}>{item}</Button>
